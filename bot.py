2a57b3a01b208fc6e0e3d0cde440a39f1d35e2bd
BOLT4L <Eyobwasihun401@gmail.com>
2025-12-23 08:53:49 +0300

    sd

--- bot.py
+++ bot.py
@@ -671,28 +671,7 @@ def general_text_handler(message):
                 "rid": rid
             })
 
-            kb = types.InlineKeyboardMarkup()
-            kb.add(
-                types.InlineKeyboardButton(
-                    "📦 Existing Food",
-                    callback_data=json.dumps({
-                        "action": "add_food_existing",
-                        "rid": rid,
-                        "page": 0
-                    })
-                )
-            )
-            kb.add(
-                types.InlineKeyboardButton(
-                    "➕ New Food",
-                    callback_data=json.dumps({
-                        "action": "add_food_new",
-                        "rid": rid
-                    })
-                )
-            )
-
-            bot.send_message(user.id, "🍔 How do you want to add food?", reply_markup=kb)
+            bot.send_message(user.id, "🖼 Send new restaurant photo:")
             return
 
 
